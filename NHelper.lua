886374509707e2b6322b9045d88d5833b1cce7b8
Bunny <90984148+zenitrise@users.noreply.github.com>
2024-05-19 19:05:09 +0300

    Update 3.9 ver.

--- NHelper.lua
+++ NHelper.lua
@@ -1404,7 +1404,7 @@ function sampev.onShowDialog(id, style, title, b1, b2, text)
         end
     end
 
-    if id == 25526 then
+    if id == 25530 then
         if addspawn_toggle.v then
             if addspawn_waittoggle.v then
                 lua_thread.create(function()
@@ -1412,11 +1412,11 @@ function sampev.onShowDialog(id, style, title, b1, b2, text)
                     time = addspawn_wait.v * 1000
                     wait(time)
                     sampAddChatMessage(tag .. textcolor .. "Выбираю " .. warncolor .. addspawn_id.v .. textcolor .. " пункт.", tagcolor)
-                    sampSendDialogResponse(25526, 1, a, _)
+                    sampSendDialogResponse(25530, 1, a, _)
                 end)
             elseif not addspawn_waittoggle.v then 
                 local a = addspawn_id.v-1
-                sampSendDialogResponse(25526, 1, a, _)
+                sampSendDialogResponse(25530, 1, a, _)
                 sampAddChatMessage(tag .. textcolor .. "Выбираю " .. warncolor .. addspawn_id.v .. textcolor .. " пункт.", tagcolor)
             end
         end
