0bba3e69f78e1c39aa2ff0da9baa763c60edd889
Bunny <90984148+zenitrise@users.noreply.github.com>
2024-05-19 22:25:44 +0300

    Update 3.91 ver.

--- NHelper.lua
+++ NHelper.lua
@@ -10,8 +10,8 @@ local warncolor = "{9c9c9c}"
 
 ---------- Авто-Обновление ----------
 
-local script_vers = 29
-local script_vers_text = "3.9"
+local script_vers = 30
+local script_vers_text = "3.91"
 local dlstatus = require("moonloader").download_status
 local update_status = false
 local download_lib = false
